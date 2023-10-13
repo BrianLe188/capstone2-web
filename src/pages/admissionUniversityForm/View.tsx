2926284bbbd386e52145c06b93472e36fcd1ab9f
vnlm01 <vanlam8526@gmail.com>
2023-10-13 14:36:50 +0700

    add value input in 4 form

--- src/pages/admissionUniversityForm/View.tsx
+++ src/pages/admissionUniversityForm/View.tsx
@@ -1,35 +1,67 @@
 import SelectLocation from "@/components/select-location";
 import "./admissionUniversityForm.css";
 import locations from "@/assets/locations.json";
+import { useContext, useState } from "react";
+import { GlobalContext } from "@/contexts/global-context";
 
 const factorArray = [
   {
     text: "Tham dự đội tuyển thi Olympic quốc tế, cuộc thi KHKT quốc tế. Xét giải các năm 2021, 2022, 2023",
+    value: 1,
   },
   {
     text: "Giải học sinh giỏi Nhất, Nhỉ, Ba, Khuyến khích cấp Quốc gia; cấp tỉnh, thành phố trực thuộc Trung ương.",
+    value: 2,
   },
   {
     text: "Giải Nhất, Nhì, Ba, Khuyến khích trong Cuộc thì Khoa học Kỹ thuật cấp quốc gia: cấp tỉnh, thành phố trực thuộc Trung ương.",
+    value: 3,
   },
   {
     text: "Giải Nhất, Nhì, Ba, khuyến khích tại các kỷ thi tay nghề khu vực ASEAN, thi tay nghề quốc tể.",
+    value: 4,
   },
   {
     text: 'Tham gia Vòng thi tuần trong cuộc thi "Đường lên đỉnh 0lympia" trên Đài truyền hình Việt Nam.',
+    value: 5,
   },
   {
     text: "Xét tuyển thẳng đối với các học sinh tốt nghiệp các trưởng THPT Chuyên của tỉnh, thành phố trực thuộc Trung ương (trừ khối ngành Khoa học Sức khỏe).",
+    value: 6,
   },
   {
     text: "Xét tuyển thẳng Thí sinh là thành viên đội tuyển quốc gia,",
+    value: 7,
   },
   {
     text: "Xết tuyển thẳng Thí sinh là người nước ngoài tốt nghiệp THPT hoặc tương đương THPT của Việt Nam (trừ khối ngành Khoa học Sức khỏe).",
+    value: 8,
   },
 ];
 
 const AdmissionUniversityForm = () => {
+  const { genders, areas, priorities } = useContext(GlobalContext);
+  const [details, setDetails] = useState<Record<string, string | null>>({
+    fullName: null,
+    gender: null,
+    birthday: null,
+    cccd: null,
+    phonenumber: null,
+    email: null,
+    area: null,
+    priority: null,
+    factor: null,
+  });
+
+  console.log(details);
+
+  const changeHandler = (name: string, value: any) => {
+    setDetails((state) => ({
+      ...state,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="flex flex-col bg-[#f6f6f6] gap-2 pt-4 px-4">
       <h1 className="text-[#A62823] font-semibold text-3xl">
@@ -50,41 +82,64 @@ const AdmissionUniversityForm = () => {
           <label htmlFor="">
             Họ và tên (<span className="text-[#A9161C]">*</span>)
           </label>
-          <input type="text" />
-          <div className="flex gap-2 items-center">
-            <input type="radio" name="gender" id="" />
-            <label htmlFor="">Nam</label>
-          </div>
-          <div className="flex gap-2 items-center">
-            <input type="radio" name="gender" id="" />
-            <label htmlFor="">Nữ</label>
-          </div>
+          <input
+            value={details.fullName || ""}
+            type="text"
+            onChange={(e) => changeHandler("fullName", e.target.value)}
+          />
+          {genders.map((item) => (
+            <div className="flex gap-2 items-center">
+              <input
+                type="radio"
+                name="gender"
+                value={item.id}
+                onChange={(e) => changeHandler("gender", e.target.value)}
+              />
+              <label htmlFor="">{item.name}</label>
+            </div>
+          ))}
         </div>
         <div className="flex gap-2">
           <div className="flex gap-2">
             <label htmlFor="">
               Ngày sinh (<span className="text-[#A9161C]">*</span>)
             </label>
-            <input type="text" />
+            <input
+              type="date"
+              value={details.birthday || ""}
+              onChange={(e) => changeHandler("birthday", e.target.value)}
+            />
           </div>
           <div className="flex gap-2">
             <label htmlFor="">
               Số CMND/CCCD (<span className="text-[#A9161C]">*</span>)
             </label>
-            <input type="text" />
+            <input
+              type="text"
+              value={details.cccd || ""}
+              onChange={(e) => changeHandler("cccd", e.target.value)}
+            />
           </div>
         </div>
         <div className="flex gap-2">
           <label htmlFor="">
             Số điện thoại (<span className="text-[#A9161C]">*</span>)
           </label>
-          <input type="text" />
+          <input
+            type="text"
+            value={details.phonenumber || ""}
+            onChange={(e) => changeHandler("phonenumber", e.target.value)}
+          />
         </div>
         <div className="flex gap-2">
           <label htmlFor="">
             Email (<span className="text-[#A9161C]">*</span>)
           </label>
-          <input type="text" />
+          <input
+            type="email"
+            value={details.email || ""}
+            onChange={(e) => changeHandler("email", e.target.value)}
+          />
         </div>
         <div className="flex gap-2">
           <label htmlFor="">
@@ -152,16 +207,32 @@ const AdmissionUniversityForm = () => {
             <label htmlFor="">
               Khu vực (<span className="text-[#A9161C]">*</span>)
             </label>
-            <select name="" id="" className="flex-1">
-              <option value=""></option>
+            <select
+              value={details.area || ""}
+              className="flex-1"
+              onChange={(e) => changeHandler("area", e.target.value)}
+            >
+              {areas.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.name}
+                </option>
+              ))}
             </select>
           </div>
           <div className="flex gap-2">
             <label htmlFor="">
               Đối tượng (<span className="text-[#A9161C]">*</span>)
             </label>
-            <select name="" id="" className="flex-1">
-              <option value=""></option>
+            <select
+              value={details.priority || ""}
+              className="flex-1"
+              onChange={(e) => changeHandler("priority", e.target.value)}
+            >
+              {priorities.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.name}
+                </option>
+              ))}
             </select>
           </div>
         </div>
@@ -172,7 +243,13 @@ const AdmissionUniversityForm = () => {
           <div className="flex flex-col gap-4">
             {factorArray.map((item, index) => (
               <div className="flex gap-2 items-center" key={index}>
-                <input type="radio" name="factor" id="" />
+                <input
+                  type="radio"
+                  name="factor"
+                  id=""
+                  value={item.value}
+                  onChange={(e) => changeHandler("factor", e.target.value)}
+                />
                 <label htmlFor="">{item.text}</label>
               </div>
             ))}
